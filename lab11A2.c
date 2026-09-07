a6ba9ac40a6c1e8f0e7231ef2dce265978b59ebd
IT26101877 <it26101877@my.sliit.lk>
2026-09-07 20:21:43 +0530

    Lab 11 activity 2

--- lab11A2.c
+++ lab11A2.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
-typedef struct{
+typedef struct{  //defining the structs
 	float width;
 	float length;
 	}rectangle_t;
-rectangle_t r;
-void print_area();
+
+void print_update(rectangle_t *r);  //function prototype
 
 int main(){
-	printf("Enter the width: );
-	scanf("%f",&b.width);
+	rectangle_t r;
+	printf("Enter the width: ");  //get the inputs
+	scanf(" %f",&r.width);
+
+	printf("Enter the length: ");
+	scanf(" %f",&r.length);
+
+	print_update(&r); //call the function
+	return 0;
+}
+void print_update(rectangle_t *r){  //the function using a pointer to the struct
+	printf("Updated Rectangle - Width: %f, Height: %f\n",r->width,r->length);
+
+}
 
 	
